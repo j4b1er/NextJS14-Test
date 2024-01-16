fd1bae5062b0d74488c655505dad791245ab5d7c
Javier Pardo <antonioj_2803@hotmail.com>
2024-01-16 17:56:08 -0500

    Added sidebar

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,5 +1,4 @@
-import { Montserrat, Roboto } from 'next/font/google';
+import { Lusitana, Montserrat, Roboto } from 'next/font/google';
 
 export const montserrat = Montserrat({ subsets: ['latin'] });
-
-export const roboto = Roboto({ weight: ['300', '500', '900'] });
+export const lusitana = Lusitana({ weight: ['400', '700'] });
