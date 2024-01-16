71df3ccbab06576026bed2a739c7ac35fc13ee5d
Javier Pardo <antonioj_2803@hotmail.com>
2024-01-16 17:34:07 -0500

    Added pages

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Montserrat, Roboto } from 'next/font/google';
+
+export const montserrat = Montserrat({ subsets: ['latin'] });
+
+export const roboto = Roboto({ weight: ['300', '500', '900'] });
