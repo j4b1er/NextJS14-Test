2493e270433c2351ebb5fb6556d2f09b952e4ccd
Javier Pardo <antonioj_2803@hotmail.com>
2024-01-19 13:23:12 -0500

    Added DB thru Vercel and pulling data

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,4 +1,7 @@
 import { Lusitana, Montserrat, Roboto } from 'next/font/google';
 
 export const montserrat = Montserrat({ subsets: ['latin'] });
-export const lusitana = Lusitana({ weight: ['400', '700'] });
+export const lusitana = Lusitana({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+});
